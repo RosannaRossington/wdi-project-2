efc6939ce1f563a97f9c5faf85893b8456566a12
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-22 14:53:40 +0100

    added name and image to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, length: { in: 3..50 }
+  validates :last_name, length: { in: 3..50 }
+
 end
