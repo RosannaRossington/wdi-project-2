67181c8d3a5ae5df1606d9834b21bf04af6c8c21
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-23 14:23:59 +0100

    Gastly gem saving a screenshot

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -11,18 +11,19 @@ class Recommendation < ActiveRecord::Base
 # current_user needs to input url(text)
 # Rcommendation model uses the method to save the url and create a screengrab using gastly
 # gastly downloads/saves in file and to product_url variable
+# self.url - is get user input
 
-  # before_save :get_product_url
+  before_save :get_product_url
 
   # private
   #   def add_default_status
   #       self.status = "pending" if self.status.nil?
   #   end
 
-  # private
-  #   def get_product_url
-  #     self.product_url = Gastly.capture(self.url, 'public/<current_user.product_url>.png')
-  #   end
+  private
+    def get_product_url
+      self.product_url = Gastly.capture(self.url, 'public/products/<current_user.product_url>.png')
+    end
 
 
 end
