db0cecdec11dd7a3508df99bf82c9444e8f997ca
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-23 10:56:30 +0100

    Added Controller for recommendations

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,3 +1,6 @@
 class Recommendation < ActiveRecord::Base
   belongs_to :user
+
+  validates :message, presence: true
+  validates :product_url, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'statics#home'
   resources :users, only: [:index, :show, :edit]
+  resources :recommendations
 end
