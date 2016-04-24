f2d8215ef981b1a3d9d152370db78bc347b87933
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-25 00:26:29 +0100

    landing page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'statics#home'
+  root 'statics#landing_page'
   resources :users, only: [:index, :show, :edit]
   resources :recommendations
   get "/inbox", to: "recommendations#inbox"
   get "/outbox", to: "recommendations#outbox"
+  get "/home", to: "statics#home"
 end
