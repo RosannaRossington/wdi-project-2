951b3679f5dae018a3779f19f6c7f9a59b3fa7d9
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-26 16:27:18 +0100

    hovering over users will take away info and just show username

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -10,10 +10,6 @@ class RecommendationsController < ApplicationController
     @recommendation = Recommendation.find(params[:id])
     @sender = User.find(@recommendation.sender_id)
 
-    # change comment user_id to User username
-    # @comment = Comment.find(params[:id])
-    # @commenter = User.find(@comment.user_id)
-    
   end
   # Set up a controller action for this rec form, so that an rec can be created and assigned to a sender, a receiver and a product url correctly. 
  
