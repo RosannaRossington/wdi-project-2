6d7077dd970638be692319184850e2263e3e595c
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-26 10:13:45 +0100

    Added configure_permitted_parameters to ApplicationController to register new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
   def after_sign_in_path_for(resource)
     home_path
   end
+
+  protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :image])
+      
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :image])
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :username, presence: true, uniqueness: true
-  validates :first_name, length: { in: 3..50 }
-  validates :last_name, length: { in: 3..50 }
+  # validates :username, presence: true, uniqueness: true
+  # validates :first_name, length: { in: 3..50 }
+  # validates :last_name, length: { in: 3..50 }
 
  
 end
