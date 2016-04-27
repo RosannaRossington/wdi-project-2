ff3109731fd28dbdca80a8e2215d5a5556493e04
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-27 12:22:43 +0100

    Can not access user profile unless signed in

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'statics#landing_page'
-  resources :users, only: [:index, :show, :edit]
   resources :recommendations do
       resources :comments
   end
   get "/inbox", to: "recommendations#inbox"
   get "/outbox", to: "recommendations#outbox"
   get "/home", to: "statics#home"
+
+  authenticate :user do
+    resources :users, only: [:new, :create, :edit, :update, :destroy, :show]
+  end
+  resources :users, only: [:index]
   
 end
