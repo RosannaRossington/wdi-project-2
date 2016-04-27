48fd776c82ffeceb444c80ed872ed237bd125b01
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-27 15:58:16 +0100

    created user.js for hover feature on users page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,19 +17,6 @@
 //= require_tree .
 
 
-$(function(){ $(document).foundation(); 
-var colors = ["blue", "pink", "green"]
-var color = colors[Math.floor(Math.random()*colors.length)]
-
-  $('.profile-card').hover(function(){
-    $(this).css("background-color",color);
-    $(this).find("h2").removeClass('hidden')
-  }, function(){ 
-    $(this).css("background-color", "initial")
-    $(this).find("h2").addClass('hidden')
-  });
-});
-
 
 
 
