6647cc73e20c9f839e843bb33d6e5590ecf9b69e
RosannaRossington <rosanna_rossington@outlook.com>
2016-04-29 10:51:43 +0100

    Database dropped

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.serve_static_assets = true
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
